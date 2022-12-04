8af898cf880c786f341e1b2e83e9df5501a99f4d
João Pedro Brito Silva <contato.joaocomp@gmail.com>
2022-12-04 14:33:20 -0300

    feat: cache directory

--- autoencoder.py
+++ autoencoder.py
@@ -1,10 +1,13 @@
 from models.autoencoder import build_autoencoder, build_autoencoder_callbacks
-from loaders.nga_dataset import nga_dataset_generator, download_nga_dataset, load_nga_dataset
+from loaders.nga_dataset import nga_dataset_generator, load_nga_dataset
+
+import matplotlib.pyplot as plt
+import pandas as pd
 import argparse
 
 DEFAULT_BATCH_SIZE = 16
 DEFAULT_TRAINING_STEPS_PER_EPOCH = 450
-DEFAULT_TRAINING_EPOCHS = 1000
+DEFAULT_TRAINING_EPOCHS = 2
 
 
 def parse_args():
@@ -33,11 +36,12 @@ if __name__ == '__main__':
     encoder.summary()
     decoder.summary()
 
-    autoencoder.fit(nga_dataset_generator(args.dataset, args.batch, mode='training', split=0.85),
-                    epochs=args.epochs,
-                    steps_per_epoch=args.steps,
-                    callbacks=build_autoencoder_callbacks(args.dataset),
-                    validation_data=(validation_images, validation_images),
-                    verbose=True)
+    history = autoencoder.fit(nga_dataset_generator(args.dataset, args.batch, mode='training', split=0.85),
+                              epochs=args.epochs, steps_per_epoch=args.steps,
+                              callbacks=build_autoencoder_callbacks(args.dataset),
+                              validation_data=(validation_images, validation_images), verbose=True)
 
-
+    pd.DataFrame(history.history).plot(figsize=(8, 5))
+    plt.gca()
+    plt.grid(True)
+    plt.show()
